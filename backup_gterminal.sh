b86fa345034025a8ce1a4e2bcaef16e373b2c620
nati <nwtbekele@gmail.com>
2024-07-25 10:10:20 -0500

    Safe directory change

--- backup_gterminal.sh
+++ backup_gterminal.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 dconf dump /org/gnome/terminal/ > ~/dotfiles/gterminal.preferences
 
-(cd ~/dotfiles; git add gterminal.preferences; git commit -m "Backup latest terminal preferences"; git push;)
+(cd ~/dotfiles || exit; git add gterminal.preferences; git commit -m "Backup latest terminal preferences"; git push;)
