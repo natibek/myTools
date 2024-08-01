cc8d00c115a1f0072053706ae2ca575d3a9e5484
nati <nwtbekele@gmail.com>
2024-07-31 21:54:47 -0500

    Show how many commts branch is behind

--- local_gits.py
+++ local_gits.py
@@ -24,14 +24,33 @@ class readable_dir(argparse.Action):
             )
 
 
-def check_git_status(git_dir, git_name, verbose, all, untracked, modified):
+def commits_behind(git_dir: str, cur_branch: str) -> int:
+    """Check how many commits you are behind."""
+
+    subprocess.call("git fetch origin".split(" "), cwd=git_dir)
+    commits_count = subprocess.check_output(
+        f"git rev-list --left-right --count {cur_branch}...origin/{cur_branch}".split(
+            " "
+        ),
+        text=True,
+        cwd=git_dir,
+    )[:-1].split("\t")
+    return int(commits_count[1])
+
+
+def git_unpushed_files(git_dir: str) -> list[str]:
     files = subprocess.check_output(
         ["git", "status", "--porcelain"],
         text=True,
         cwd=git_dir,
     ).split("\n")
 
-    files = [file.strip() for file in files if file]
+    return [file.strip() for file in files if file]
+
+
+def check_git_status(git_dir, git_name, verbose, all, untracked, modified, show_behind):
+
+    files = git_unpushed_files(git_dir)
 
     cur_branch = subprocess.check_output(
         ["git", "branch", "--show-current"],
@@ -39,8 +58,12 @@ def check_git_status(git_dir, git_name, verbose, all, untracked, modified):
         cwd=git_dir,
     )[:-1]
 
+    num_behind = commits_behind(git_dir, cur_branch) if show_behind else 0
+
     home_path = os.path.expanduser("~")
-    if len(files) == 0:
+    file_display_text = failure(f"{git_name}") + f"<{cur_branch}>" + failure("-> ")
+
+    if len(files) == 0 and num_behind == 0:
         if all:
             print(
                 f"{git_dir.replace(home_path, '~')}: {success(git_name)}<{cur_branch}>"
@@ -50,16 +73,22 @@ def check_git_status(git_dir, git_name, verbose, all, untracked, modified):
     modified_count = sum(1 for file in files if file.startswith("M"))
     untracked_count = sum(1 for file in files if file.startswith("?"))
 
-    if (modified_count and modified) or (untracked_count and untracked):
+    if (
+        (modified_count and modified)
+        or (untracked_count and untracked)
+        or num_behind > 0
+    ):
         file_display_text = failure(f"{git_name}") + f"<{cur_branch}>" + failure("-> ")
         print_text = f"{git_dir.replace(home_path, '~')}: {file_display_text}"
     else:
         return 0
 
     if modified_count > 0 and modified:
-        print_text += f"{failure('M')}odified: {failure(str(modified_count))} "
+        print_text += f"{failure('M')}odified:{failure(str(modified_count))} "
     if untracked_count > 0 and untracked:
-        print_text += f"{failure('U')}ntracked: {failure(str(untracked_count))} "
+        print_text += f"{failure('U')}ntracked:{failure(str(untracked_count))} "
+    if num_behind > 0:
+        print_text += f"{failure('B')}ehind:{failure(str(num_behind))}"
 
     print(print_text)
 
@@ -119,6 +148,12 @@ def main():
         default=False,
         help="Whether to show all the github repos. Default shows ones with unpushed changes.",
     )
+    parser.add_argument(
+        "--show-behind",
+        action="store_true",
+        default=False,
+        help="Whether to also show how many commits a local repo is behind the origin.",
+    )
 
     behind_type = parser.add_mutually_exclusive_group()
     behind_type.add_argument(
@@ -151,7 +186,13 @@ def main():
     for git_name, git_dir in gits:
         if git_name not in exclude:
             exit_code |= check_git_status(
-                git_dir, git_name, args.verbose, args.all, untracked, modified
+                git_dir,
+                git_name,
+                args.verbose,
+                args.all,
+                untracked,
+                modified,
+                args.show_behind,
             )
 
     if exit_code == 9:
