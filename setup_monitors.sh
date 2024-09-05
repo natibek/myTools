7c47d7f62e0325df98e0b18f077ca29b726627ec
natibek <nwtbekele@gmail.com>
2024-09-05 16:22:44 -0500

    fix monitor setup script

--- setup_monitors.sh
+++ setup_monitors.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-for monitor in $(xrandr --listmonitors | awk '$4 != "eDP" && $4 !="" {print $4}')
+for monitor in $(xrandr | grep -w "connected" | awk '$1 !~ /eDP/ && $1 !="" {print $1}')
 do
   xrandr --output $monitor --auto --above eDP
 done
+
+eDP_monitor=$(xrandr | grep -w "connected" | awk '$1 ~ /eDP/ {print $1}')
+host=$(neofetch --off | awk '$1 ~ /Host/ {print $2}')
+
+if [ "$host" = "Levono" ]; then
+  xrandr --output "$eDP_monitor" --mode 1920x1080
+fi
+
